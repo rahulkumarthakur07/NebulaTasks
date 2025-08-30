1475d08f7ea64c57cb94acb0564ff0ea7e38ff07
Rahul Sharma <rahulsharma982856@gmail.com>
2025-08-30 22:16:17 +0545

    Fix import case for Foreground.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Background from './components/Background'
-import Foreground from './components/Foreground.Jsx'
+import Foreground from './components/ForeGround'
 
 
 const App = () => {
